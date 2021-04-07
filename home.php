6e75573a2de22b04f0da07ca07bb1bce1efad430
RezaKgit <rezak6177@gmail.com>
2021-04-07 15:03:40 +0700

    menambahkan file home.php

---
+++ home.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Coba Remote 2</title>
+</head>
+<body>
+    <h2>Reza Kurniawan</h2>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo quae exercitationem ea autem placeat dolore corporis ipsa, a iste porro aspernatur? Totam praesentium accusamus similique amet minus quia sit aperiam.</p>
+
+    <ul>
+        <ul>Nama Buah : </ul>
+        <li>Anggur</li>
+        <li>Jambu</li>
+        <li>Apel</li>
+        <li>Jeruk</li>
+    </ul>
+</body>
+</html>
